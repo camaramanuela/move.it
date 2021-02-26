eeaca832cfe99737aab9bf7c320eb13454db2a54
Manuela Bognar <manuelabcamara@gmail.com>
2021-02-26 00:03:44 -0300

    completed challenge

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/manuelabognar.png" alt="Manuela Bognar" />
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Manuela Bognar</strong>
         <p>
           <img src="icons/level.svg" alt="level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
